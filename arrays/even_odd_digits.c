d842b102fb8ee7e7321d58e3250798d93844abb3
AshfakAhmath <ashfakahmath57@gmail.com>
2026-01-11 17:26:26 +0530

    Solved even odd digits using array

--- arrays/even_odd_digits.c
+++ arrays/even_odd_digits.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int main()
+{
+    int N;
+    // Read number of elements
+    scanf("%d", &N);
+
+    int arr[N];
+
+    // Read array elements
+    for(int i = 0; i < N; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    // Check even or odd
+    for(int i = 0; i < N; i++){
+        if(arr[i] % 2 == 0){
+            printf("%d is Even\n", arr[i]);
+        }
+        else{
+            printf("%d is Odd\n", arr[i]);
+        }
+    }
+
+    return 0;
+}

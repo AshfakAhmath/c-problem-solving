7f8bcd68a4fca115af3488a102ee7a32008bda27
AshfakAhmath <ashfakahmath57@gmail.com>
2026-01-14 16:54:56 +0530

    array element passing

---
+++ Passing Array Elements to Functions/array_element_passing.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void arrFunction(int value)
+{
+    printf("%d ", arr);
+}
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    for(int i = 0; i<5; i++){
+        arrFunction(arr[i]);
+    }
+    return 0;
+}
